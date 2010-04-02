0bbb4f3675dc1cf40ad28b51ce33563019559946
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2010-04-02 14:21:38 +0000

    Simplified significantly by using commonPluginSettings; use PREFIX, PORT and QSIZE macros
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@10514 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- iocBoot/iocAdsc/st.cmd
+++ iocBoot/iocAdsc/st.cmd
@@ -4,28 +4,32 @@ errlogInit(20000)
 dbLoadDatabase("$(AREA_DETECTOR)/dbd/adscApp.dbd")
 adscApp_registerRecordDeviceDriver(pdbbase) 
 
+epicsEnvSet("PREFIX", "13ADCS1:")
+epicsEnvSet("PORT",   "ADSC1")
+epicsEnvSet("QSIZE",  "20")
+
 #
 # adscConfig(const char *portName, const char *modelName)
 #   portName   asyn port name
 #   modelName  ADSC detector model name; must be one of "Q4", "Q4r", "Q210",
 #              "Q210r", "Q270", "Q315", or "Q315r"
 #
-adscConfig("ADSC1","Q210r")
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/ADBase.template","P=13ADSC1:,R=det1:,PORT=ADSC1,ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/adsc.template","P=13ADSC1:,R=det1:,PORT=ADSC1,ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template","P=13ADSC1:,R=det1:,PORT=ADSC1,ADDR=0,TIMEOUT=1")
+adscConfig("$(PORT)","Q210r")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/ADBase.template","P=$(PREFIX),R=det1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/adsc.template",  "P=$(PREFIX),R=det1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template","P=$(PREFIX),R=det1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 
-#asynSetTraceMask("ADSC1",0,255)
+#asynSetTraceMask("$(PORT)",0,255)
 
 set_requestfile_path("./")
 set_savefile_path("./autosave")
 set_requestfile_path("$(AREA_DETECTOR)/ADApp/Db")
 set_pass0_restoreFile("auto_settings.sav")
 set_pass1_restoreFile("auto_settings.sav")
-save_restoreSet_status_prefix("13ADSC1:")
-dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db","P=13ADSC1:")
+save_restoreSet_status_prefix("$(PREFIX)")
+dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db","P=$(PREFIX)")
 
 iocInit()
 
 # save things every thirty seconds
-create_monitor_set("auto_settings.req",30,"P=13ADSC1:,R=det1:")
+create_monitor_set("auto_settings.req",30,"P=$(PREFIX),R=det1:")
