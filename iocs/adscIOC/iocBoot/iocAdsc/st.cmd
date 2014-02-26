01deae8b29bef10d9550bf0368cd1971ca7d33d0
Mark Rivers <rivers@cars.uchicago.edu>
2014-02-25 21:36:02 -0600

    Changes for new layout

--- iocs/adscIOC/iocBoot/iocAdsc/st.cmd
+++ iocs/adscIOC/iocBoot/iocAdsc/st.cmd
@@ -1,7 +1,7 @@
 < envPaths
 errlogInit(20000)
 
-dbLoadDatabase("$(AREA_DETECTOR)/dbd/adscApp.dbd")
+dbLoadDatabase("$(TOP)/dbd/adscApp.dbd")
 adscApp_registerRecordDeviceDriver(pdbbase) 
 
 epicsEnvSet("PREFIX", "13ADCS1:")
@@ -18,15 +18,15 @@ epicsEnvSet("NCHANS", "2048")
 #              "Q210r", "Q270", "Q315", or "Q315r"
 #
 adscConfig("$(PORT)","Q210r")
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/ADBase.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/adsc.template",  "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(ADCORE)/db/ADBase.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(ADADSC)/db/adsc.template",  "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(ADCORE)/db/NDFile.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 
 #asynSetTraceMask("$(PORT)",0,255)
 
 set_requestfile_path("./")
 set_savefile_path("./autosave")
-set_requestfile_path("$(AREA_DETECTOR)/ADApp/Db")
+set_requestfile_path("$(ADCORE)/ADApp/Db")
 set_pass0_restoreFile("auto_settings.sav")
 set_pass1_restoreFile("auto_settings.sav")
 save_restoreSet_status_prefix("$(PREFIX)")
