9182a3f27dd3a1b079f8f4b2427e0787df4bd4fc
Mark Rivers <rivers@cars.uchicago.edu>
2015-03-29 17:40:22 -0500

    Don't load NDFile.template, loaded from adsc.template

--- iocs/adscIOC/iocBoot/iocAdsc/st.cmd
+++ iocs/adscIOC/iocBoot/iocAdsc/st.cmd
@@ -29,7 +29,6 @@ epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
 #
 adscConfig("$(PORT)","Q210r")
 dbLoadRecords("$(ADADSC)/db/adsc.template",  "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(ADCORE)/db/NDFile.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 
 #asynSetTraceMask("$(PORT)",0,255)
 
